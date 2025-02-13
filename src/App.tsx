4beeede888462314f4effed43feab5bf58894575
Maique Moraes <maiqued.18@gmail.com>
2025-02-13 01:50:25 -0300

    feat: create loading store and add it to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { AppRoutes } from "./routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Loader } from "./components/loader.tsx";
+import { Home } from "./pages/home";
+import { TruckList } from "./pages/truck-list";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <Loader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/caminhoes" element={<TruckList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
